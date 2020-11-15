0bd1808abe8af3d592905d1cab65a76bb1632ff5
Gaurav Sinha <gaurav.sinha@oracle.com>
2020-11-15 22:49:14 +0530

    Mirror tree puzzle

---
+++ src/com/puzzle/tree/MirrorTree.java
@@ -0,0 +1,39 @@
+package com.puzzle.tree;
+/**
+ * Date 11/15/2020
+ *
+ * @author gauravenrich
+ * Mirror Tree Program
+ */
+public class MirrorTree {
+
+    public static void main(String args[]){
+        BinaryTree bt = new BinaryTree(new Node(1));
+        bt.root.left = new Node(2);
+        bt.root.right = new Node(3);
+
+        bt.root.right.left = new Node(6);
+        bt.root.right.right = new Node(7);
+        bt.root.right.right.right = new Node(71);
+        LevelOrderTraversal.levelOrderTraversal(bt.root);
+        System.out.println();
+        mirrorOfTree(bt.root);
+        LevelOrderTraversal.levelOrderTraversal(bt.root);
+    }
+
+    private static void mirrorOfTree(Node root) {
+        if(root==null)
+            return;
+        else
+        {
+            mirrorOfTree(root.left);
+            mirrorOfTree(root.right);
+            Node temp=root.left;
+            root.left=root.right;
+            root.right=temp;
+
+
+        }
+
+    }
+}
