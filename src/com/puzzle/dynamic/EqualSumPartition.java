e0b18a941e1bda8df9c47c0c84340ace7fd45302
Gaurav Sinha <gaurav.sinha@oracle.com>
2020-12-30 00:49:26 +0530

    Equal Sum Partition Problem

--- src/com/puzzle/dynamic/EqualSumPartition.java
+++ src/com/puzzle/dynamic/EqualSumPartition.java
@@ -52,38 +52,5 @@ public class EqualSumPartition {
         return t[n][w];
     }
 
-    public static boolean subSetSum(int sum, int wt[], int n) {
-
-        boolean[][] t = new boolean[n + 1][sum + 1];
-
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < sum + 1; j++) {
-                if (i == 0) {
-                    t[i][j] = false;
-                }
-            }
-        }
-        for (int i = 0; i < n + 1; i++) {
-
-            for (int j = 0; j < sum + 1; j++) {
-                if (j == 0) {
-                    t[i][j] = true;
-                }
-            }
-        }
-
-        for (int i = 1; i < n + 1; i++) {
-
-            for (int j = 1; j < sum + 1; j++) {
-                if (wt[i - 1] <= j) {
-                    t[i][j] = t[i - 1][j - wt[i - 1]] || t[i - 1][j];
-                } else {
-                    t[i][j] = t[i - 1][j];
-                }
-
-            }
-        }
-        return t[n][sum];
-    }
 }
 
