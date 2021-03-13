c509615daa3b7656fc91f11e051173f2528c8237
Gaurav Sinha <gaurav.sinha@oracle.com>
2021-03-13 08:49:17 +0530

    Sort an array of 0s, 1s and 2s

---
+++ src/com/puzzle/array/DutchNationalFlagProblem.java
@@ -0,0 +1,48 @@
+package com.puzzle.array;
+
+/**
+ *  Date 03/13/2020
+ *  @author gauravenrich
+ *  Sort an array of 0s, 1s and 2s
+ */
+public class DutchNationalFlagProblem {
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+        int arr_size = arr.length;
+        sort012(arr, arr_size);
+        System.out.println("Array after segregation ");
+        for(int i = 0; i < arr_size; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    private static void sort012(int[] a, int n) {
+        int start = 0;
+        int end = n - 1;
+        int mid = 0;
+        while (mid <= end) {
+            switch (a[mid]) {
+                case 0: {
+                    swapElement(a,start,mid);
+                    start++;
+                    mid++;
+                    break;
+                }
+                case 1:{
+                    mid++;
+                    break;
+                }
+                case 2:{
+                    swapElement(a,mid,end);
+                    end--; // processing is not done yet so we are not doing mid-- as it can be 1 that can be swapped
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void swapElement(int[] a, int i, int j) {
+        int temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+    }
+}
