4998fcd2326df82b5a409b0bd6d0f1b164786981
Gaurav Sinha <gaurav.sinha@oracle.com>
2021-03-13 07:28:07 +0530

    Changing date

--- src/com/puzzle/array/FindElementSortedRotatedArray.java
+++ src/com/puzzle/array/FindElementSortedRotatedArray.java
@@ -1,2 +1,21 @@
-package com.puzzle.array;public class FindElementSortedRotatedArray {
+package com.puzzle.array;
+
+public class FindElementSortedRotatedArray {
+
+    public static void main(String[] args){
+        int[] array = {21, 27, 34, 47, 54, 66};
+        int index = findElement(array);
+        System.out.println("Index is "+index);
+
+    }
+
+    private static int findElement(int[] array) {
+        int pivot=findPivot(array);
+        return -1;
+        
+    }
+
+    private static int findPivot(int[] array) {
+        return -1;
+    }
 }
