fe5b7e62fc85da0eb5e000cbce827fc0e14075f1
Gaurav Sinha <gaurav.sinha@oracle.com>
2022-06-10 08:40:00 +0530

    Optimizing start of loop program

--- src/com/puzzle/linkedlist/StartOfLoop.java
+++ src/com/puzzle/linkedlist/StartOfLoop.java
@@ -8,11 +8,6 @@ package com.puzzle.linkedlist;
  */
 public class StartOfLoop {
 
-    static class Node {
-        int key;
-        Node next;
-    };
-
     static Node newNode(int key) {
         Node temp = new Node();
         temp.key = key;
@@ -20,26 +15,13 @@ public class StartOfLoop {
         return temp;
     }
 
-
-
-    // Function to detect and remove loop
-// in a linked list that may contain loop
     static Node detectAndRemoveLoop(Node head) {
-// If list is empty or has
-// only one node without loop
         if (head == null || head.next == null)
             return null;
 
         Node slow = head, fast = head;
-
-// Move slow and fast 1
-// and 2 steps ahead
-// respectively.
         slow = slow.next;
         fast = fast.next.next;
-
-// Search for loop using
-// slow and fast pointers
         while (fast != null &&
                 fast.next != null) {
             if (slow == fast)
@@ -47,19 +29,16 @@ public class StartOfLoop {
             slow = slow.next;
             fast = fast.next.next;
         }
-
 // If loop does not exist
         if (slow != fast)
             return null;
-
-// If loop exists. Start slow from
-// head and fast from meeting point.
         slow = head;
         while (slow != fast) {
             slow = slow.next;
-            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+            }
         }
-
         return slow;
     }
 
@@ -79,5 +58,10 @@ public class StartOfLoop {
             System.out.print("Loop does not exist");
         else
             System.out.print("Loop starting node is " + res.key);
+    }
+
+    static class Node {
+        int key;
+        Node next;
     }
 }
