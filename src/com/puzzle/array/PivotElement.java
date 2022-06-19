59eb34f1af9c9b7a73d449bc365d72c01a6d19f0
Gaurav Sinha <gaurav.sinha@oracle.com>
2022-06-19 08:55:30 +0530

    Pivot search

--- src/com/puzzle/array/PivotElement.java
+++ src/com/puzzle/array/PivotElement.java
@@ -1,9 +1,10 @@
 package com.puzzle.array;
 
 /**
- *  Date 11/25/2020
- *  @author gauravenrich
- *  Pivot Element
+ * Date 11/25/2020
+ *
+ * @author gauravenrich
+ * Pivot Element
  */
 public class PivotElement {
 
@@ -17,21 +18,21 @@ public class PivotElement {
         if (a == null || a.length == 0) {
             return -1;
         }
-     // Case when array is not rotated. Then first index is the pivot
+        // Case when array is not rotated. Then first index is the pivot
         if (a.length == 1 || a[0] < a[a.length - 1]) {
             return 0;
         }
-         int start = 0;
-            int end = a.length - 1;
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (a[mid] > a[mid + 1] && mid < a.length - 1)
-                    return mid + 1;
-                else if (a[mid] <= a[start])
-                    end = mid - 1;
-                else
-                    start = mid + 1;
-            }
-            return -1;
+        int start = 0;
+        int end = a.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (a[mid] > a[mid + 1] && mid < a.length - 1)
+                return mid + 1;
+            else if (a[mid] <= a[start])
+                end = mid - 1;
+            else
+                start = mid + 1;
         }
+        return -1;
+    }
 }
--- src/com/puzzle/array/SortedRotatedPivotSearch.java
+++ src/com/puzzle/array/SortedRotatedPivotSearch.java
@@ -1,21 +1,67 @@
 package com.puzzle.array;
 
+/**
+ * * Date 19/06/2022
+ *  * @author gauravenrich
+ * Search an element in a sorted and rotated array
+ */
 public class SortedRotatedPivotSearch {
 
-    public static void main(String[] args){
-        int[] array = {21, 27, 34, 47, 54, 66};
-        int index = findElement(array);
-        System.out.println("Index is "+index);
+    public static void main(String[] args) {
+        int[] array = {21, 27, 34, 47, 2, 6};
+        int index = findElement(array, 222);
+        System.out.println("Index is " + index);
 
     }
 
-    private static int findElement(int[] array) {
-        int pivot=findPivot(array);
-        return -1;
-        
+    private static int findElement(int[] a, int num) {
+        int pivot = findPivot(a);
+        System.out.println("Pivot is "+pivot);
+        if (a[pivot] == num) {
+            return pivot;
+        }
+        if (pivot == 0) {
+            return binarySearch(a, num, 0, a.length - 1);
+        } else if (num < a[0]) {
+            return binarySearch(a, num, pivot, a.length - 1);
+        } else {
+            return binarySearch(a, num, 0, pivot);
+        }
     }
 
-    private static int findPivot(int[] array) {
+    static int binarySearch(int[] a, int num, int start, int end) {
+        if(end>start){
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (a[mid] == num) {
+            return mid;
+        } else if (a[mid] < num) {
+            return binarySearch(a, num, mid + 1, end);
+        } else
+            return binarySearch(a, num, start, mid - 1);
+    }
+
+    private static int findPivot(int[] a) {
+        if (a == null || a.length == 0) {
+            return -1;
+        }
+        // Case when array is not rotated. Then first index is the pivot
+        if (a.length == 1 || a[0] < a[a.length - 1]) {
+            return 0;
+        }
+        int start = 0;
+        int end = a.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (a[mid] > a[mid + 1] && mid < a.length - 1) {
+                return mid + 1;
+            } else if (a[start] >= a[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
         return -1;
     }
 }
