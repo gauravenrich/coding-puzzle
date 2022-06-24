d895ea515bdb0fe48ed1eeabbc6c7fdcc604ce3f
Gaurav Sinha <gaurav.sinha@oracle.com>
2022-06-25 00:11:05 +0530

    BST Insert program

---
+++ src/com/puzzle/bst/BasicBstProgram.java
@@ -0,0 +1,15 @@
+package com.puzzle.bst;
+
+public class BasicBstProgram {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+        tree.inorder();
+    }
+}
--- src/com/puzzle/bst/BinarySearchTree.java
+++ src/com/puzzle/bst/BinarySearchTree.java
@@ -1,2 +1,54 @@
-package com.puzzle.bst;public class BinarySearchTree {
+package com.puzzle.bst;
+
+/**
+ * @author gsinha
+ * 25/06/2022
+ * BST Program
+ */
+public class BinarySearchTree {
+    Node root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int val) {
+        root = insertRec(root, val);
+    }
+
+    private Node insertRec(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.key > val) {
+            root.left = insertRec(root.left, val);
+        } else if (root.key < val) {
+            root.right = insertRec(root.right, val);
+        }
+        return root;
+    }
+
+    public void inorder() {
+        inorderRec(root);
+    }
+
+    private void inorderRec(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorderRec(root.left);
+        System.out.print(root.key + " ");
+        inorderRec(root.right);
+    }
+
+    static class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
 }
