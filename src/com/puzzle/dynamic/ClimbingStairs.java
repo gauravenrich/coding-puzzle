aadbbd18a4e94f2bd99fdc90d7e4da13f307a889
Gaurav Sinha <gaurav.sinha@oracle.com>
2022-06-25 09:32:02 +0530

    Number if ways for climbing stairs

---
+++ src/com/puzzle/dynamic/ClimbingStairs.java
@@ -0,0 +1,30 @@
+package com.puzzle.dynamic;
+
+/**
+ * @author gsinha
+ * 26/06/2022
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ */
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+        System.out.println("Number of ways for climbing steps " + climbingStairs.climbStairs(5));
+    }
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
