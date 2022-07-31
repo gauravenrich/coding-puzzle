7153f420ca589a1e1709e60afb72cb0ef0dc1bda
Gaurav Sinha <gaurav.sinha@oracle.com>
2022-07-31 10:20:29 +0530

    Adding comment and formatting the code

--- src/com/puzzle/bit/FindOnlySetBitPosition.java
+++ src/com/puzzle/bit/FindOnlySetBitPosition.java
@@ -6,22 +6,27 @@ package com.puzzle.bit;
  * As we see there's only one set bit
  * and it's in Position 2 and thus the
  * Output 2.
+ *
+ * @author gsinha
+ *
+ * 31 july 2022
  */
 public class FindOnlySetBitPosition {
     public static void main(String[] args) {
-        System.out.println(findPosition(5));
+        System.out.println(findPosition(4));
     }
+
     static int findPosition(int n) {
-        int count=0;
-        if(n==0)
+        int count = 0;
+        if (n == 0)
             return -1;
-        for(int i=0;i<32;i++){
-            if((n&1)!=0){
-                if(count>0)
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) != 0) {
+                if (count > 0)
                     return -1;
-                count=i+1;
+                count = i + 1;
             }
-            n=n>>1;
+            n = n >> 1;
         }
 
         return count;
--- src/com/puzzle/bit/NonRepeatingNumbers.java
+++ src/com/puzzle/bit/NonRepeatingNumbers.java
@@ -1,33 +1,52 @@
 package com.puzzle.bit;
 
+import java.util.Arrays;
+
+/**
+ * @author gsinha
+ *
+ * arr[] = {1, 2, 3, 2, 1, 4}
+ * Output:
+ * 3 4
+ * Explanation:
+ * 3 and 4 occur exactly once.
+ *
+ *  31 july 2022
+ */
 public class NonRepeatingNumbers {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3, 2, 1, 4};
+        NonRepeatingNumbers obj = new NonRepeatingNumbers();
+        System.out.println(Arrays.toString(obj.singleNumber(arr)));
     }
+
     public int[] singleNumber(int[] nums) {
         int result = 0;
         int first = 0;
         int second = 0;
-        int []res = new int[2];
+        int[] res = new int[2];
 
-        for(int e : nums)
-        {
+        for (int e : nums) {
             result = result ^ e;
         }
 
-        int rightBit = result&(-result);
+        int rightBit = result & ~(result - 1);
+        /*
+         e.g 7 & ~(6)= 111 & 001 = 001
+         ~(110)=001
+         */
         System.out.println(rightBit);
         for (int num : nums) {
             int cur = num & rightBit;
             if (cur == 1) {
-                first = first ^ num;
+                first = first ^ num;  // collect all numbers with 1 in the end
             } else {
-                second = second ^ num;
+                second = second ^ num; // collect all numbers with 0 in the end
             }
         }
 
-        res[0]=first;
-        res[1]=second;
+        res[0] = first;
+        res[1] = second;
         return res;
     }
 }
